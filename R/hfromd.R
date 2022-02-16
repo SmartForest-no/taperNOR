fc5e08ea32a89a3efae525cfdd3130f3e3479bf2
Johannes Rahlf <johannes.rahlf@nibio.no>
2022-02-16 13:17:41 +0100

    Added lm estimation of start values for optimization

--- R/hfromd.R
+++ R/hfromd.R
@@ -19,33 +19,46 @@ hfromd<-function(d,h,sp="spruce",output="h"){
     d<-d[h>=0.5]
     h<-h[h>=0.5]
   }
+
+  #estimate starting values
+  if(length(d)>=2){
+  st<-
+    c(
+      predict(lm(d~h),newdata = data.frame(h=1.3)),
+      predict(lm(h~d),newdata = data.frame(d=0))
+    )
+  } else {
+    st<-c(25,20)
+  }
+
   result<-
-    stats::optim(par=c(25,20),
+    stats::optim(par=st,
                  fn= function(x_o,h_o,d_o,sp_o){
                    return(
                      abs(
                        mean(
                          (d_o-
-                           taperNO(
-                             h=h_o,
-                             dbh=x_o[2],
-                             h_top=x_o[1],
-                             sp=sp_o))
-                         )
+                            taperNO(
+                              h=h_o,
+                              dbh=x_o[2],
+                              h_top=x_o[1],
+                              sp=sp_o))
+                       )
                      )
-                     )
+                   )
                  },
                  h_o=h,
                  d_o=d,
                  sp_o=sp#,
                  # method = "L-BFGS-B" ,lower=c(1.3,5),upper=c(50,150)
-                 )
+    )
+  names(result$par)<-c("h","d")
 
   if(output=="h") {
     return(result$par[1])
   } else if(output=="d"){
     return(result$par[2])
- } else if(output=="all"){
+  } else if(output=="all"){
     return(result)
   } else {
     return(result$par)
