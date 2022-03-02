94c282b64180df0a50c4a9c9aaf73a72d19e31a1
Johannes Rahlf <johannes.rahlf@nibio.no>
2022-03-02 11:30:58 +0100

    added DBH position and text to plot_taper

--- R/plot_taper.R
+++ R/plot_taper.R
@@ -19,6 +19,8 @@ plot_taper<-function(dbh,h_top,sp="spruce"){
     xlab="h (m)",
     ylab="d (cm)"
   )
+  points(1.3,taperNO(1.3,dbh,h_top,sp),pch=16)
+  mtext(paste0("DBH= ",round(dbh,2)," cm; H= ",round(h_top,2)," m"),adj=0,line=0)
 
 
 }
